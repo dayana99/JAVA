1a9ead76804f2c49640c4b1c039ced2045d38b92
dayana99 <38000046+dayana99@users.noreply.github.com>
2019-04-11 00:49:38 +0800

    Create server1.java

---
+++ server1.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+class TCPServer {
+ public static void main(String argv[]) throws Exception {
+  String clientSentence,replySentence,justifySentence;
+  String reply,answer;
+  ServerSocket welcomeSocket = new ServerSocket(6789);
+
+reply = "Waalaikumusalam";
+answer = "Are u Muslim?";
+
+  while (true) {
+   Socket connectionSocket = welcomeSocket.accept();
+   BufferedReader inFromClient =
+    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+   clientSentence = inFromClient.readLine();
+   System.out.println("Received: " + clientSentence);
+  if (clientSentence.equalsIgnoreCase("Assalamualaikum"))
+{
+replySentence = reply;
+outToClient.writeBytes(reply));
+}
+
+else
+{
+justifySentence = answer;
+outToClient.writeBytes(answer));
+}
+
+  }
+ }
+}
